d368fbcbf4759ad666c41640cd6c6a2d122014a3
dumamoiseev <duma.moiseev@gmail.com>
2016-09-15 23:11:59 +0300

    hello mir + value of cube

--- sandbox/src/main/java/HW.java
+++ sandbox/src/main/java/HW.java
@@ -2,8 +2,19 @@ public class HW {
 
 	public static void main(String[] args) {
 
-System.out.println("Hello, world!");
+		hello("Mir");
+		hello("Mir");
+		hello("Mir");
 
+		double l = 10;
+		System.out.println("Объем куба с ребром "+ l + " = " + area(l));
 	}
- 
+ 	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+	public static double area(double l) {
+		return l * l * l
+		;
+	}
+
 }
