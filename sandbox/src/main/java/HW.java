3ed5964bcf6b82579420455908e4aa6a5e50d036
dumamoiseev <duma.moiseev@gmail.com>
2016-09-16 09:34:26 +0300

    function distance  added to class Point

--- sandbox/src/main/java/HW.java
+++ sandbox/src/main/java/HW.java
@@ -10,13 +10,11 @@ public class HW {
 		p2.y=4;
 
 
-		System.out.println("расстояние между точками" + " = " + distance(p1,p2));
+		System.out.println("расстояние между точками" + " = " + Point.distance(p1,p2));
 	}
 
 
-	public static double distance(Point p1,Point p2) {
-		return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-	}
+
 }
 
 
